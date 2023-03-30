4aa1103d93a84bda1eaf67fe9eb7f51b38d0444c
Ahmed <30933484+Ahmed-Manai@users.noreply.github.com>
2023-03-30 07:57:31 +0000

    Exp of Factory Method DP

---
+++ Creational/C-Sharp/FactoryMethod/Program.cs
@@ -0,0 +1,63 @@
+﻿public interface IProduct
+{
+    public string Operation();
+}
+
+public class ConcreteProductA : IProduct
+{
+    public string Operation()
+    {
+        return "result of Concrete Product A";
+    }
+}
+
+public class ConcreteProductB : IProduct
+{
+    public string Operation()
+    {
+        return "result of Concrete Product B";
+    }
+}
+
+public abstract class Creator
+{
+    public abstract IProduct FactoryMethod(); 
+    
+    public string SomeOperation()
+    {
+        var product = FactoryMethod();
+        var res = "Creator: " + product.Operation();
+        return res;
+    }
+
+}
+public class ConcreteCreatorA : Creator
+{
+    public override IProduct FactoryMethod()
+    {
+        return new ConcreteProductA();
+    }
+}
+public class ConcreteCreatorB : Creator
+{
+    public override IProduct FactoryMethod()
+    {
+       return new ConcreteProductB();
+    }
+    
+}
+
+
+public class Program 
+{
+    public static void Main(string[] args)
+    {
+        ClientCode(new ConcreteCreatorA());
+        ClientCode(new ConcreteCreatorB());
+    }
+
+    public static void ClientCode(Creator creator)
+    {
+        Console.WriteLine("Clietn:" + creator.SomeOperation());
+    }
+}
